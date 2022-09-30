b3066103258695d3fd718d2936f535d629936b1a
Shantanu <shantanu.suwarnkar@kcl.ac.uk>
2022-09-30 11:28:37 +0100

    primes

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        add = True
+        for j in range(2,i):
+            if i%j == 0:
+                add = False
+                break
+        if add:
+            list.append(i)
+        i += 1
     return list
+
+print(primes(2))
